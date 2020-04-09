f4a64db484909cee0ab3ddad5950ddeda02923dd
Vivek Gupta <vivek.gupta8983@gmail.com>
2020-04-09 13:44:41 +0530

    Update

--- lamp.sh
+++ lamp.sh
@@ -187,19 +187,10 @@ cd /var/www/html/wordpress
 #create wp config
 cp wp-config-sample.php wp-config.php
 #set database details with perl find and replace
-perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
-perl -pi -e "s/username_here/$dbuser/g" wp-config.php
-perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+sed -e “s/database_name_here/$dbname/g” wp-config.php
+sed -e “s/username_here/$dbuser/g” wp-config.php
+sed “s/password_here/$dbpass/g” wp-config.php
 
-#set WP salts
-perl -i -pe'
-  BEGIN {
-    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
-    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
-  }
-  s/put your unique phrase here/salt()/ge
-' wp-config.php
 
 #create uploads folder and set permissions
 mkdir wp-content/uploads
